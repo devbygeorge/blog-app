f641bae99d071f74ce19ed19f31936c333a74d42
devbygeorge <giopasieshvili2@gmail.com>
2024-11-26 23:03:26 +0400

    Update Mongodb connect

--- blog-node-app/index.js
+++ blog-node-app/index.js
@@ -24,10 +24,7 @@ app.use("/api/profile", profileRoutes);
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
